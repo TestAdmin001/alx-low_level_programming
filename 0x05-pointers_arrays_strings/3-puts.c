56f227c39f93b81191dbe18087fb989c32f9ba1d
Olaitan A Odufuwa <64588415+TestAdmin001@users.noreply.github.com>
2022-09-19 20:33:59 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
-#include "nain.h"
-
+#include "main.h"
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
+ * _puts - function that prints a string, followed by a new line
+ * @str: declaration of *str and paramters for the function _puts
+ * Return: Always 0.
  */
 void _puts(char *str)
 {
-	int i;
+	int c;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (c = 0; str[c] != 0; c++)
 	{
-		_putchar(str[i]);
+		_putchar(str[c]);
 	}
 	_putchar('\n');
 }
